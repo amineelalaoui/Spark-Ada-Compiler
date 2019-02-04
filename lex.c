41fedfcf1fb285148b0be4240a1c179ae0918cdb
amineelalaoui <amine-elalaoui@um5s.net.ma>
2019-02-05 00:39:59 +0100

    Update lex.c

--- lex.c
+++ lex.c
@@ -5,17 +5,11 @@
 #include "lex.h"
 
 /**
-
 *
-
 * @author : Mohamed Amine EL ALAOUI
-
 * @author : KHALIL Idriss
-
 * @author : MEFTAHI Abderrahman
-
 * @author : LAATAATA Abderrazak
-
 **/
 
 #define Max_NBRE 20
@@ -23,250 +17,128 @@
 
 boolean debugLEX = TRUE;
 
-
-
-
-
 /**
  * global variables
  * 
  * */
+
 CODE_LEX LAST;
-
- Token tabToken[] = {
-
+Token tabToken[] = {
     {ABORT_TOKEN ,"abort"},
-
     {ABS_TOKEN ,"abs"},
-
     {ABSTRACT_TOKEN ,"abstract"},
-
     {ACCEPT_TOKEN ,"accept"},
-
     {ACCESS_TOKEN ,"access"},
-
     {ALIASED_TOKEN ,"aliased"},
-
     {ALL_TOKEN ,"all"},
-
     {AND_TOKEN ,"and"},
-
     {ARRAY_TOKEN ,"array"},
-
     {AT_TOKEN ,"at"},
-
     {BEGIN_TOKEN ,"begin"},
-
     {TRUE_TOKEN ,"true"},
-
     {FALSE_TOKEN ,"false"},
-
     {PRINT_TOKEN ,"print"},
-
     {READ_TOKEN ,"read"},
-
     {BODY_TOKEN ,"body"},
-
     {CASE_TOKEN ,"case"},
-
     {CONSTANT_TOKEN ,"constant"},
-
     {DECLARE_TOKEN ,"declare"},
-
     {DELAY_TOKEN ,"delay"},
-
     {DELTA_TOKEN ,"delta"},
-
     {DIGITS_TOKEN ,"digits"},
-
     {DO_TOKEN ,"do"},
-
     {ELSE_TOKEN ,"else"},
-
     {ELSIF_TOKEN ,"elsif"},
-
     {END_TOKEN ,"end"},
-
     {ENTRY_TOKEN ,"entry"},
-
     {EXCEPTION_TOKEN ,"exception"},
-
     {EXIT_TOKEN ,"exit"},
-
     {FOR_TOKEN ,"for"},
-
     {FUNCTION_TOKEN ,"function"},
-
     {GENERIC_TOKEN ,"generic"},
-
     {GOTO_TOKEN ,"goto"},
-
     {IF_TOKEN ,"if"},
-
     {IN_TOKEN ,"in"},
-
     {INTERFACE_TOKEN ,"interface"},
-
     {IS_TOKEN ,"is"},
-
     {LIMITED_TOKEN ,"limited"},
-
     {LOOP_TOKEN ,"loop"},
-
     {MOD_TOKEN ,"mod"},
-
     {NEW_TOKEN ,"new"},
-
     {NOT_TOKEN ,"not"},
-
     {NULL_TOKEN ,"null"},
-
     {OF_TOKEN ,"of"},
-
     {OR_TOKEN ,"or"},
-
     {OTHERS_TOKEN ,"others"},
-
     {OUT_TOKEN ,"out"},
-
     {OVERRIDING_TOKEN ,"overriding"},
-
     {PACKAGE_TOKEN ,"package"},
-
     {PRAGMA_TOKEN ,"pragma"},
-
     {PRIVATE_TOKEN ,"private"},
-
     {PROCEDURE_TOKEN ,"procedure"},
-
     {PROTECTED_TOKEN, "protected"},
-
     {RAISE_TOKEN ,"raise"},
-
     {RANGE_TOKEN ,"range"},
-
     {RECORD_TOKEN ,"record"},
-
     {REM_TOKEN ,"rem"},
-
     {RENAMES_TOKEN ,"renames"},
-
     {REQUEUE_TOKEN ,"requeue"},
-
     {RETURN_TOKEN ,"return"},
-
     {REVERSE_TOKEN ,"reverse"},
-
     {SELECT_TOKEN ,"select"},
-
     {SEPARATE_TOKEN ,"separate"},
-
     {SOME_TOKEN ,"some"},
-
     {SUBTYPE_TOKEN ,"subtype"},
-
     {SYNCHRONIZED_TOKEN ,"synchronized"},
-
     {TAGGED_TOKEN ,"tagged"},
-
     {TASK_TOKEN ,"task"},
-
     {TERMINATE_TOKEN ,"terminate"},
-
     {THEN_TOKEN ,"then"},
-
     {TYPE_TOKEN ,"type"},
-
     {UNTIL_TOKEN ,"until"},
-
     {USE_TOKEN ,"use"},
-
     {FIRST_TOKEN ,"first"},
-
     {LAST_TOKEN ,"last"},
-
     {WHEN_TOKEN ,"when"},
-
     {WHILE_TOKEN ,"while"},
-
     {WITH_TOKEN ,"with"},
-
     {XOR_TOKEN ,"xor"},
-
-
-
-
-
     //literals = ['&','(',')','*','+',',','-','.','/',':',';','<','=','>','|'] + ['\"','#']
-
     {EC_TOKEN ,"&"},
-
     {AFFEC_MULT_TOKEN ,"=*"},
-
     {AFFEC_DIV_TOKEN ,"=/"},
-
     //les caractere speciaux
-
     {PV_TOKEN ,";"},
-
     {DOUBLE_POINT_TOKEN ,":"},
-
     {PT_TOKEN ,"."},
-
     {PLUS_TOKEN ,"+"},
-
     {MOINS_TOKEN ,"-"},
-
     {MULT_TOKEN ,"*"},
-
     {DIV_TOKEN ,"/"},
-
     {VIR_TOKEN ,","},
-
     {EGAL_TOKEN ,"="},
-
     {DIV_AFFEC_TOKEN ,"/="},
-
     {MULT_AFFEC_TOKEN ,"*="},
-
     {AFF_TOKEN ,":="},
-
     {INF_TOKEN ,"<"},
-
     {INFEG_TOKEN ,"<="},
-
     {SUP_TOKEN ,">"},
-
     {SUPEG_TOKEN ,">="},
-
     {DIFF_TOKEN ,"<>"},
-
     {PO_TOKEN ,"("},
-
     {PF_TOKEN ,")"},
-
     {APP_TOKEN ,"'"},
-
     {DOUBLE_QUOTES_TOKEN ,"\""},
-
     {COM_TOKEN ,"--"},
-
     //{FIN_TOKEN ,EOF},
-
     //{ID_TOKEN, ID},
-
     //{NUM_TOKEN, NUM}
-
     //{FLOAT_TOKEN, FLOAT}
-
     // {STRING_TOKEN, STRING}
-
     //,{ERREUR_TOKEN, RESTE}
-
 };
 
 int nbMotRes = sizeof tabToken / sizeof tabToken[0];
-
 char Car_Cour;
 boolean follow_token = FALSE;
 boolean first;
@@ -320,420 +192,210 @@ int tailleERR = sizeof MES_ERR / sizeof MES_ERR[0];
 FILE *fl = NULL;
 
 void showCodeToken(Token token){
-
-
     switch((int)token.CODE){
-
         case ID_TOKEN:
             if(debugLEX)
                 printf("ID_TOKEN");
-
             break;
-
         case NUM_TOKEN:
-
             if(debugLEX) printf("NUM_TOKEN");
-
             break;
-
         case FLOAT_TOKEN:
-
             if(debugLEX) printf("FLOAT_TOKEN");
-
             break;
-
         case BEGIN_TOKEN:
-
             if(debugLEX) printf("BEGIN_TOKEN");
-
             break;
-
         case FALSE_TOKEN:
-
             if(debugLEX) printf("FALSE_TOKEN");
-
             break;
-
         case TRUE_TOKEN:
-
             if(debugLEX) printf("TRUE_TOKEN");
-
             break;
-        
         case PRINT_TOKEN:
-
             if(debugLEX) printf("PRINT_TOKEN");
-
             break;
-
         case READ_TOKEN:
-
             if(debugLEX) printf("READ_TOKEN");
-
             break;
-
         case END_TOKEN:
-
             if(debugLEX) printf("END_TOKEN");
-
             break;
-
         case IF_TOKEN:
-
             if(debugLEX) printf("IF_TOKEN");
-
             break;
-
         case THEN_TOKEN:
-
             if(debugLEX) printf("THEN_TOKEN");
-
             break;
-
         case WHILE_TOKEN:
-
             if(debugLEX) printf("WHILE_TOKEN");
-
             break;
-
-
-
-
-
-
-
-
-
-
-
         case PV_TOKEN:
-
             if(debugLEX) printf("PV_TOKEN");
-
             break;
-
         case PT_TOKEN:
-
             if(debugLEX) printf("PT_TOKEN");
-
             break;
-
         case PLUS_TOKEN:
-
             if(debugLEX) printf("PLUS_TOKEN");
-
             break;
-
         case MOINS_TOKEN:
-
             if(debugLEX) printf("MOINS_TOKEN");
-
             break;
-
         case MULT_TOKEN:
-
             if(debugLEX) printf("MULT_TOKEN");
-
             break;
-
         case DIV_TOKEN:
-
             if(debugLEX) printf("DIV_TOKEN");
-
             break;
-
         case VIR_TOKEN:
-
             if(debugLEX) printf("VIR_TOKEN");
-
             break;
-
         case EGAL_TOKEN:
-
             if(debugLEX) printf("EGAL_TOKEN");
-
             break;
-
         case MULT_AFFEC_TOKEN:
-
             if(debugLEX) printf("MULT_AFFEC_TOKEN");
-
             break;
-
         case DIV_AFFEC_TOKEN:
-
             if(debugLEX) printf("DIV_AFFEC_TOKEN");
-
             break;
-
         case AFF_TOKEN:
-
             if(debugLEX) printf("AFF_TOKEN");
-
             break;
-
         case INF_TOKEN:
-
             if(debugLEX) printf("INF_TOKEN");
-
             break;
-
         case INFEG_TOKEN:
-
             if(debugLEX) printf("INFEG_TOKEN");
-
             break;
-
         case SUP_TOKEN:
-
             if(debugLEX) printf("SUP_TOKEN");
-
             break;
-
         case SUPEG_TOKEN:
-
             if(debugLEX) printf("SUPEG_TOKEN");
-
             break;
-
         case DIFF_TOKEN:
-
             if(debugLEX) printf("DIFF_TOKEN");
-
             break;
-
         case PO_TOKEN:
-
             if(debugLEX) printf("PO_TOKEN");
-
             break;
-
         case PF_TOKEN:
-
             if(debugLEX) printf("PF_TOKEN");
-
             break;
-
         case FIN_TOKEN:
-
             if(debugLEX) printf("FIN_TOKEN");
-
             break;
-
         case IN_TOKEN:
-
             if(debugLEX) printf("IN_TOKEN");
-
             break;
-
         case DOUBLE_POINT_TOKEN:
-
             if(debugLEX) printf("DOUBLE_POINT_TOKEN");
-
             break;
-
         case APP_TOKEN:
-
             if(debugLEX) printf("APP_TOKEN");
-
             break;
-
         case DOUBLE_QUOTES_TOKEN:
-
             if(debugLEX) printf("DOUBLE_QUOTES_TOKEN");
-
-            break;
-        
+            break;     
         case IS_TOKEN:
-
             if(debugLEX) printf("IS_TOKEN");
-
             break;
-
         case ERREUR_TOKEN:
-
             if(debugLEX) printf("ERREUR_TOKEN");
-
             break;
-
     }
-
 }
 
-//=================================== lexical================
-
-
-
 int estBlanc(char c) {
-
     return isspace(c);
-
 }
 
 int is_underscore(){
-
     return Car_Cour =='_';
-
 }
 
 int isAccentLettre(){
-
     // if(-128<Car_Cour && Car_Cour <= -102){ // FROM Ç TO Ü
-
     if(-96<= Car_Cour && Car_Cour <= -61 ){ //éèçàùûâêîô
-
         return 1;
-
     }
-
     return 0;
-
 }
 
 void lire_Car(){
-
     Car_Cour = getc(fl);
-
 }
 
 void lire_mot(){
-
     int lonLex = 0;
-
     SYM_COUR.NOM[lonLex++] = Car_Cour;
-
     lire_Car();
-
     while (isalpha(Car_Cour) || isdigit(Car_Cour) || is_underscore()) {
-
         SYM_COUR.NOM[lonLex++] = Car_Cour;
-
         lire_Car();
-
     }
-
-
-
     if ( isAccentLettre() ) { //é à ...
-
         while (isalpha(Car_Cour) || isdigit(Car_Cour) || isAccentLettre() || is_underscore()) {
-
             SYM_COUR.NOM[lonLex++] = Car_Cour;
-
             lire_Car();
-
         }
-
         detectError(ERR_ID_INV);
-
         SYM_COUR.CODE = ERREUR_TOKEN;
-
     }
-
     else if (lonLex <= 20) {
-
         // on cherche de symbole
-
         for(int i = 0; i<nbMotRes; i++){
-
             if( ! strcasecmp(SYM_COUR.NOM, tabToken[i].NOM ) ){
-
                 SYM_COUR.CODE = tabToken[i].CODE;
-
                 break;
-
             }
-
             else{
-
                 SYM_COUR.CODE = ID_TOKEN;
-
             }
-
         }
-
     }
-
     else{
-
         detectError(ERR_ID_LONG);
-
         SYM_COUR.CODE = ERREUR_TOKEN;
-
     }
-
     //fin du mot
-
     SYM_COUR.NOM[lonLex++] = '\0';
-
     ungetc(Car_Cour, fl);
-
     if(debugLEX)    
         printf("Symcour --> %s ( ", SYM_COUR.NOM);
-
     if(debugLEX) 
         showCodeToken(SYM_COUR);
-    
     if(debugLEX) puts(" )");
-
-
-
 }
 
 void lire_nombre(){
-
     int lonLex = 0;
-
     SYM_COUR.NOM[lonLex++] = Car_Cour;
-
     lire_Car();
-
     while ( isdigit(Car_Cour) ) {
-
         SYM_COUR.NOM[lonLex++] = Car_Cour;
-
         lire_Car();
-
     }
-
     SYM_COUR.CODE = NUM_TOKEN;
-
     if ( Car_Cour == '.') { // on switch vers la lecture d'un float
-
         SYM_COUR.NOM[lonLex++] = Car_Cour;
-
         lire_Car();
-
         if ( !isdigit(Car_Cour) ) {
-
             ungetc(Car_Cour, fl);
-
             SYM_COUR.NOM[lonLex - 1] = '\0';
-
         }
-
         else{
-
             SYM_COUR.CODE = FLOAT_TOKEN;
-
             while( isdigit(Car_Cour) ){
-
                 SYM_COUR.NOM[lonLex++] = Car_Cour;
-
                 lire_Car();
-
             }
-
         }
-
     }
-
-
-
     if ( isalpha(Car_Cour) || isAccentLettre() || is_underscore() ) { // identificateur qui commence par des digits
-
         while ( isalnum(Car_Cour) || isAccentLettre() || is_underscore() ) {
-
             SYM_COUR.NOM[lonLex++] = Car_Cour;
 
             lire_Car();
